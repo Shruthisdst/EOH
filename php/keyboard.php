00410da807737f464b309dcd3aa3f249c9b262a3
Shruthisdst <shruthitr.nayak@gmail.com>
2015-09-19 10:15:02 +0530

    Commit No. 10 - special characters order changed in serach

--- php/keyboard.php
+++ php/keyboard.php
@@ -2,30 +2,30 @@
 	<div class="softkeyHead">Soft keyboard to insert special symbols</div>
 	<div class="keys tline lline" id="A" onclick="InsertText('Ā')">Ā</div>
 	<div class="keys tline" id="a" onclick="InsertText('ā')">ā</div>
-	<div class="keys lline" id="S" onclick="InsertText('Ś')">Ś</div>
-	<div class="keys" id="s" onclick="InsertText('ś')">ś</div>
-	<div class="keys lline" id="U" onclick="InsertText('Ū')">Ū</div>
-	<div class="keys" id="u" onclick="InsertText('ū')">ū</div>
-	<div class="keys lline" id="SA" onclick="InsertText('Ṣ')">Ṣ</div>
-	<div class="keys" id="sa" onclick="InsertText('ṣ')">ṣ</div>
 	<div class="keys lline" id="I" onclick="InsertText('Ī')">Ī</div>
 	<div class="keys" id="i" onclick="InsertText('ī')">ī</div>
-	<div class="keys lline" id="N" onclick="InsertText('Ṅ')">Ṅ</div>
-	<div class="keys" id="n" onclick="InsertText('ṅ')">ṅ</div>
+	<div class="keys lline" id="U" onclick="InsertText('Ū')">Ū</div>
+	<div class="keys" id="u" onclick="InsertText('ū')">ū</div>
 	<div class="keys lline" id="R" onclick="InsertText('Ṛ')">Ṛ</div>
 	<div class="keys" id="r" onclick="InsertText('ṛ')">ṛ</div>
-	<div class="keys lline" id="T" onclick="InsertText('Ṭ')">Ṭ</div>
-	<div class="keys" id="t" onclick="InsertText('ṭ')">ṭ</div>
-	<div class="keys lline" id="NA" onclick="InsertText('Ṇ')">Ṇ</div>
-	<div class="keys" id="na" onclick="InsertText('ṇ')">ṇ</div>
-	<div class="keys lline" id="D" onclick="InsertText('Ḍ')">Ḍ</div>
-	<div class="keys" id="d" onclick="InsertText('ḍ')">ḍ</div>
 	<div class="keys lline" id="M" onclick="InsertText('Ṁ')">Ṁ</div>
 	<div class="keys" id="m" onclick="InsertText('ṁ')">ṁ</div>
-	<div class="keys lline" id="NB" onclick="InsertText('Ñ')">Ñ</div>
-	<div class="keys" id="nb" onclick="InsertText('ñ')">ñ</div>
 	<div class="keys lline" id="H" onclick="InsertText('Ḥ')">Ḥ</div>
 	<div class="keys" id="h" onclick="InsertText('ḥ')">ḥ</div>
+	<div class="keys lline" id="N" onclick="InsertText('Ṅ')">Ṅ</div>
+	<div class="keys" id="n" onclick="InsertText('ṅ')">ṅ</div>
+	<div class="keys lline" id="NB" onclick="InsertText('Ñ')">Ñ</div>
+	<div class="keys" id="nb" onclick="InsertText('ñ')">ñ</div>
+	<div class="keys lline" id="T" onclick="InsertText('Ṭ')">Ṭ</div>
+	<div class="keys" id="t" onclick="InsertText('ṭ')">ṭ</div>
+	<div class="keys lline" id="D" onclick="InsertText('Ḍ')">Ḍ</div>
+	<div class="keys" id="d" onclick="InsertText('ḍ')">ḍ</div>
+	<div class="keys lline" id="NA" onclick="InsertText('Ṇ')">Ṇ</div>
+	<div class="keys" id="na" onclick="InsertText('ṇ')">ṇ</div>
+	<div class="keys lline" id="S" onclick="InsertText('Ś')">Ś</div>
+	<div class="keys" id="s" onclick="InsertText('ś')">ś</div>
+	<div class="keys lline" id="SA" onclick="InsertText('Ṣ')">Ṣ</div>
+	<div class="keys" id="sa" onclick="InsertText('ṣ')">ṣ</div>
 	<div class="keys lline" id="L" onclick="InsertText('Ḷ')">Ḷ</div>
 	<div class="keys" id="l" onclick="InsertText('ḷ')">ḷ</div>
 </div>
