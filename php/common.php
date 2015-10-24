6e91bc7812ea01bd006bca1405e20d2794df3c90
Shruthisdst <shruthitr.nayak@gmail.com>
2015-10-24 19:41:40 +0530

    Commit No. 19 - A to D Word list updated with images

--- php/common.php
+++ php/common.php
@@ -17,5 +17,37 @@ function isValidWord($word)
 	return is_array($word) ? false : true;
 }
 
+function replaceHeadings($xmlVal)
+{
+	$xmlVal = preg_replace('/<strong>(.*)<\/strong>/', "<span class=\"boldText\">$1</span>", $xmlVal);
+	$xmlVal = preg_replace('/<h1>(.*)<\/h1>/', "<p class=\"normalText\">$1</p>", $xmlVal);
+	$xmlVal = preg_replace('/<h2>(.*)<\/h2>/', "<p class=\"italicText\">$1</p>", $xmlVal);
+	$xmlVal = preg_replace('/<p type="poem">(.*)<\/p>/', "<p class=\"poem\">$1</p>", $xmlVal);
+	$xmlVal = preg_replace('/<h3>(.*)<\/h3>/', "<p class=\"italicBold\">$1</p>", $xmlVal);
+	$xmlVal = preg_replace_callback('/<ref>(.*?)<\/ref>/', "EncodeAndLower", $xmlVal);
+	return($xmlVal);
+}
+function EncodeAndLower($paraMeters)
+{
+	$tmp = $paraMeters[1];
+	$tmp = strtolower($tmp);
+	$tmp = preg_replace('/Ā/','ā',$tmp);
+	$tmp = preg_replace('/Ś/','ś',$tmp);
+	$tmp = preg_replace('/Ū/','ū',$tmp);
+	$tmp = preg_replace('/Ṣ/','ṣ',$tmp);
+	$tmp = preg_replace('/Ī/','ī',$tmp);
+	$tmp = preg_replace('/Ṅ/','ṅ',$tmp);
+	$tmp = preg_replace('/Ṛ/','ṛ',$tmp);
+	$tmp = preg_replace('/Ṭ/','ṭ',$tmp);
+	$tmp = preg_replace('/Ṇ/','ṇ',$tmp);
+	$tmp = preg_replace('/Ḍ/','ḍ',$tmp);
+	$tmp = preg_replace('/Ṁ/','ṁ',$tmp);
+	$tmp = preg_replace('/Ñ/','ñ',$tmp);
+	$tmp = preg_replace('/Ḥ/','ḥ',$tmp);
+	$tmp = preg_replace('/Ḷ/','ḷ',$tmp);
+	$tmp = preg_replace('/Ṝ/','ṝ',$tmp);
+	$tmp = "<a class=\"refWord\" href=\"displayword.php?word=" . $tmp . "\">". $paraMeters[1]. "</a>";
+	return $tmp;
+}
 
 ?>
--- php/search-result.php
+++ php/search-result.php
@@ -12,6 +12,8 @@ $searchWord = rtrim($searchWord);
 $searchWord = preg_replace("/[ ]+/", " ", $searchWord);
 $searchWord = preg_replace("/^ /", "", $searchWord);
 $searchWord = preg_replace("/ $/", "", $searchWord);
+$footNote = '';
+
 if($searchWord=='')
 {
 	$searchWord = '. *';
@@ -102,13 +104,8 @@ if(!empty($_POST['chk_list']))
 			
 			echo '<div class="word">';
 			echo '<div class="whead">';
-			foreach ($xmlObj->head->image as $image)
-			{
-				if($image != '')
-				{
-					echo "<span class='crossref'><a href='img/thumbs/$image' data-lightbox='imgae-".$id."' data-title='". $xmlObj->head->word . "'><img src='img/main/$image' alt='Figure:" . $xmlObj->head->word . "' /></a></span><br />";
-				}
-			}
+			$word = $row['word'];
+			
 			echo '<span class="engWord clr1">'.$xmlObj->head->word;
 			foreach ($xmlObj->head->alias as $alias)
 			{
@@ -134,7 +131,6 @@ if(!empty($_POST['chk_list']))
 				echo '</div>';
 			}
 			echo '<div class="wBody">';
-			
 			foreach ($xmlObj->description->children() as $child)
 			{
 				$xmlVal = $child->asXML();
@@ -160,15 +156,30 @@ if(!empty($_POST['chk_list']))
 						}
 					}
 				}
-				$xmlVal = preg_replace('/<subhead>(.*)<\/subhead>/', "<p class=\"subhead\">$1</p>", $xmlVal);
-				$xmlVal = preg_replace_callback('/<ref>(.*?)<\/ref>/', "EncodeAndLower", $xmlVal);
-				echo $xmlVal;
-				
+				$xmlVal = replaceHeadings($xmlVal);
+				if(preg_match('#<figure>#', $xmlVal, $match))
+				{
+					echo "<span class='crossref'><a href='img/thumbs/$word.png' data-lightbox='imgae-".$id."' data-title='". $xmlObj->head->word . "'><img src='img/main/$word.png' alt='Figure:" . $xmlObj->head->word . "' /></a></span><br />";
+				}
+				if(preg_match('#<aside>(.*)<\/aside>#', $xmlVal, $match))
+				{
+					$xmlVal = preg_replace('/<aside>(.*?)<\/aside>/', "<span class=\"fntsymbol\">*</span>", $xmlVal);
+					echo $xmlVal;
+					$footNote = $match[1];
+				}
+				else
+				{
+					echo $xmlVal;
+				}
+			}
+			if($footNote != '')
+			{
+				echo "<div class=\"FootNote\"><span class=\"fntsymbol\">*</span>$footNote</div>";
 			}
 			echo '</div>';
 			//echo '<div class="wBody">'. $xmlObj->description->asXML() . '</div>';
-			
 			echo '</div>';
+			$footNote = '';
 		}
 	}
 	else
@@ -179,30 +190,6 @@ if(!empty($_POST['chk_list']))
 else
 {
 	echo '<a href="search.php" class="sml clr2">Please  Select Atleast One Option.</a>';
-}
-
-
-function EncodeAndLower($paraMeters)
-{
-	$tmp = $paraMeters[1];
-	$tmp = strtolower($tmp);
-	$tmp = preg_replace('/Ā/','ā',$tmp);
-	$tmp = preg_replace('/Ś/','ś',$tmp);
-	$tmp = preg_replace('/Ū/','ū',$tmp);
-	$tmp = preg_replace('/Ṣ/','ṣ',$tmp);
-	$tmp = preg_replace('/Ī/','ī',$tmp);
-	$tmp = preg_replace('/Ṅ/','ṅ',$tmp);
-	$tmp = preg_replace('/Ṛ/','ṛ',$tmp);
-	$tmp = preg_replace('/Ṭ/','ṭ',$tmp);
-	$tmp = preg_replace('/Ṇ/','ṇ',$tmp);
-	$tmp = preg_replace('/Ḍ/','ḍ',$tmp);
-	$tmp = preg_replace('/Ṁ/','ṁ',$tmp);
-	$tmp = preg_replace('/Ñ/','ñ',$tmp);
-	$tmp = preg_replace('/Ḥ/','ḥ',$tmp);
-	$tmp = preg_replace('/Ḷ/','ḷ',$tmp);
-	$tmp = preg_replace('/<span style="color: red">(.*)<\/span>/','\1',$tmp);
-	$tmp = "<a class=\"refWord\" href=\"displayword.php?word=" . $tmp . "\">". $paraMeters[1]. "</a>";
-	return $tmp;
 }
 ?>
             </div> <!-- cd-container -->
